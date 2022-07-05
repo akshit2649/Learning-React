3e3241fdc9bfd9adae9d8c480043ed1b90398889
Akshit Thakur <akshit2649@gmail.com>
2022-07-05 19:30:13 +0530

    Modified

--- food_ordering_app/src/components/header/Navbar.jsx
+++ food_ordering_app/src/components/header/Navbar.jsx
@@ -3,7 +3,7 @@ import CartButton from "./CartButton";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between bg-red-600 px-[100px] h-[80px] items-center">
+    <div className="flex justify-between bg-red-600 px-[100px] h-[80px] items-center w-[100%] ">
       <p className="text-white font-bold text-3xl ">ReactMeals</p>
       <CartButton />
     </div>
