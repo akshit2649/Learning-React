4a52f680e2b3944a2df7522ca7564633eddb0591
Akshit Thakur <akshit2649@gmail.com>
2022-07-09 14:22:31 +0530

    Added Cart Reducer

--- food_ordering_app/src/store/cartProvider.js
+++ food_ordering_app/src/store/cartProvider.js
@@ -1,13 +1,41 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
-const cartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
+const cartReducer = (state, action) => {
+  //concat adds item to the array but unlike push, concat returns a new array
+  if (action.type === "ADD") {
+    const updateItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    return {
+      items: updateItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
+const cartProvider = (props) => {
+  //[State snapshot, function which allows to dispatch an action]
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
   const removeItemFromCartHandler = (id) => {};
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
