37a63b96130e98c280668018e451c3b18963ce00
Akshit Thakur <akshit2649@gmail.com>
2022-07-17 18:23:35 +0530

    Checkout form

--- food_ordering_app/src/components/Cart/Checkout.js
+++ food_ordering_app/src/components/Cart/Checkout.js
@@ -1,7 +1,48 @@
+import { useRef } from "react";
 import React from "react";
+import classes from "./Checkout.module.css";
 
-const Checkout = () => {
-  return <div>Checkout</div>;
+const Checkout = (props) => {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
+  const enteredName = nameInputRef.current.value;
+  const enteredStreet = streetInputRef.current.value;
+  const enteredPostalCode = postalCodeInputRef.current.value;
+  const enteredCity = cityInputRef.current.value;
+
+  const confirmHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <form className={classes.form} onSubmit={confirmHandler}>
+      <div className={classes.control}>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" ref={nameInputRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" ref={streetInputRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" ref={postalCodeInputRef} />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInputRef} />
+      </div>
+      <div className={classes.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        {/* type of button does not submit the form */}
+        <button className={classes.submit}>Confirm</button>
+      </div>
+    </form>
+  );
 };
 
 export default Checkout;
